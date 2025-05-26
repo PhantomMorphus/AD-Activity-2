8a52a50e171902fb316da8817c171b34f34eaa6c
PhantomMorphus <louiejohnpamilar@gmail.com>
2025-05-26 10:47:50 +0800

    Feat: (Redirection) - Change of file of certain source codes

--- index.php
+++ index.php
@@ -3,13 +3,30 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>boring.exe</title>
+    <link rel="stylesheet" type="text/css" href="assets/css/homePage.css">
+    <title>Armored Core 6 - Home</title>
 </head>
-
-<nav>
-    
-</nav>
 <body>
-    
+    <div class="dynamic-bg"></div>
+    <script src="assets/js/dynamicBG.js"></script>
+    <header>
+        <h1>Armored Core 6: All Armored Cores</h1>
+    </header>
+    <main>
+        <section class="intro-section">
+            <h2>Welcome to the Armored Core 6 Database</h2>
+            <p>
+                Discover all Armored Cores featured in Armored Core 6! Use the faction buttons to filter by allegiance.
+            </p>
+
+        </section>
+
+    </main>
+    <footer class="faction-buttons">
+        <a href="balam.php">Balam</a>
+        <a href="arquebus.php">Arquebus</a>
+        <a href="rubicon.php">Rubicon Liberation Front</a>
+        <a href="rad.php">RaD</a>
+    </footer>
 </body>
 </html>
